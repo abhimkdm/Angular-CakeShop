a7549daf2b2c3440b12dd1306b4d3304c625f893
Raghu K <kp.raghu911@gmail.com>
2020-03-15 12:55:27 +0530

    created login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, MenuBarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [LoginComponent]
